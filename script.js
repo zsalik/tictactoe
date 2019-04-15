c75f599dabed277c6c596c1330dd29ef3ac1cbd0
ZS <zsalik@gmail.com>
2019-04-14 22:57:25 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var origBoard;
+const huPlayer = "0";
+const aiPlayer = "X";
+const winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+];
+
+const cells = document.querySelectorAll(".cell");
+startGame();
+
+function startGame() {
+  document.querySelector(".endgame").style.display = "none";
+  origBoard = Array.from(Array(9).keys());
+}
