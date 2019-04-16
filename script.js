98646e31ae0e38a0f0c5181106425a21c0c7a396
ZS <zsalik@gmail.com>
2019-04-16 14:53:29 -0400

    wrote checkWin function

--- script.js
+++ script.js
@@ -39,3 +39,14 @@ function turn(squareId, player) {
   let gameWon = checkWin(origBoard, player);
   if (gameWon) gameOver(gameWon);
 }
+
+function checkWin(board, player) {
+  let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), []);
+  let gameWon = null;
+  for (let [index, win] of winCombos.entries()) {
+    if (win.every(elem => plays.indexOf(elem) > -1)) {
+      gameWon = { index: index, player: player };
+      break;
+    }
+  }
+}
